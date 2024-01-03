fa360fb017abed685b55cf0dbca4fa971243b7aa
Pablo Gutierrez <pablovgv@proton.me>
2024-01-02 23:54:17 -0300

    Finished base pong game

--- Pong/main.py
+++ Pong/main.py
@@ -1,24 +1,137 @@
 import pygame as pg
+from objects import Paddle, Ball
 
+win_x = 1280
+win_y = 720
+fps = 60
 
-win_x = 800
-win_y = 600
-font = pg.font.Font('freesansbold.ttf', 20)
+paddle_space = 15
+paddle_width = win_x // 60
+paddle_height = win_y // 4
+paddle_center = (win_y // 2) - (paddle_height // 2)
+paddle_speed = 12
+paddle_color = "white"
+
+ball_radius = win_x // 40
+ball_speed = 8
+ball_color = "white"
+
+score_x = win_x // 5
+score_y = win_y // 5
+score_color = "white"
+
+#print(f"[SCORE POSITION] X:{score_x}, Y:{score_y}")
 
 pg.init()
 clock = pg.time.Clock()
 
-win = pg.display
-win.set_mode((win_x, win_y))
-win.set_caption("Pong?")
+flags = pg.RESIZABLE #| pg.FULLSCREEN
+win = pg.display.set_mode((win_x, win_y), flags)
+pg.display.set_caption("Pong?")
+font = pg.font.Font(None, 40)
+
 
 
 def game():
     run = True
+    global win, win_x, win_y
+    l_point = False
+    r_point = False
+    
+    paddle_left = Paddle(win, win_y, paddle_space, paddle_center, paddle_width, paddle_height, paddle_speed, paddle_color)
+    paddle_right = Paddle(win, win_y, win_x - paddle_width - paddle_space, paddle_center, paddle_width, paddle_height, paddle_speed, paddle_color)
+    ball = Ball(win, win_x, win_y, win_x // 2, win_y // 2, ball_radius, ball_speed, ball_color, 1, -1)
+
+    paddle_list = [paddle_left, paddle_right]
+
+    p_l_score, p_r_score = 0, 0
+    p_l_yMult, p_r_yMult = 0, 0
+
     while run:
+        
+        win.fill("black")
+
+
         for event in pg.event.get():
+            
             if event.type == pg.QUIT:
                 run = False
+            elif event.type == pg.VIDEORESIZE:
+                #win = pg.display.set_mode(event.size, flags)
+                win_x, win_y = event.size
+            
+        
+        
+        ## Keybinds
+        keys = pg.key.get_pressed()
+        
+        esc = keys[pg.K_ESCAPE]
+        p_l_up = keys[pg.K_w]
+        p_l_down = keys[pg.K_s]
+        p_r_up = keys[pg.K_o]
+        p_r_down = keys[pg.K_l]
+
+        if p_l_up and p_l_down:
+            p_l_yMult = 0
+        elif p_l_up:
+            p_l_yMult = -1
+        elif p_l_down:
+            p_l_yMult = 1
+        else:
+            p_l_yMult = 0
+        
+        if p_r_up and p_r_down:
+            p_r_yMult = 0
+        elif p_r_up:
+            p_r_yMult = -1
+        elif p_r_down:
+            p_r_yMult = 1
+        else:
+            p_r_yMult = 0
+
+        if esc:
+            run = False
 
 
-pg.quit()
+        for paddle in paddle_list:
+            #if pg.Rect.colliderect(ball.getRect(), paddle.getRect()):
+                #ball.hit()
+            if pg.Rect.colliderect(ball.getRect(), paddle_left.getRect()) and ball.getDir() == -1:
+                ball.hit()
+            if pg.Rect.colliderect(ball.getRect(), paddle_right.getRect()) and ball.getDir() == 1:
+                ball.hit()
+
+
+        paddle_left.update(p_l_yMult)
+        paddle_right.update(p_r_yMult)
+        point = ball.update()
+
+
+        if point == -1:
+            p_r_score += 1
+        elif point == 1:
+            p_l_score += 1
+        if point:
+            ball.reset(ball_speed)
+
+        paddle_left.display()
+        paddle_right.display()
+        ball.display()
+        #center_point = pg.draw.circle(win, "red", (win_x // 2, win_y // 2), 1)
+
+
+        paddle_left.displayScore(p_l_score, score_x, score_y, font, score_color)
+        paddle_right.displayScore(p_r_score, win_x - score_x, score_y, font, score_color)
+
+        #print(p_l_score, p_r_score)
+
+        pg.display.update()
+
+        l_point, r_point = False, False
+
+        clock.tick(fps)
+
+    pg.quit()
+
+if __name__ == "__main__":
+    game()
--- Pong/objects.py
+++ Pong/objects.py
@@ -1,8 +1,13 @@
 import pygame as pg
 
+def speed_conv(x):
+    new_speed = x ** 0.5
+    return new_speed
+
 class Paddle:
-    def __init__(self, window, posx, posy, width, height, speed, color):
+    def __init__(self, window, window_height, posx, posy, width, height, speed, color):
         self.window = window
+        self.window_height = window_height
         self.posx = posx
         self.posy = posy
         self.width = width
@@ -10,52 +15,98 @@ class Paddle:
         self.speed = speed
         self.color = color
         
-        self.paddleRect = pg.Rect(posx, posy, width, height)
-        self.paddle = pg.draw.rect(window, self.color, self.paddleRect)
+        self.paddleRect = pg.Rect(posx, posy, self.width, self.height)
+        self.paddle = pg.draw.rect(self.window, self.color, self.paddleRect)
 
     def display(self):
-        self.paddle = pg.draw.rect(window, self.color, self.paddleRect)
+        self.paddle = pg.draw.rect(self.window, self.color, self.paddleRect)
     
-    def update(self, yMult, window_height):
+    def update(self, yMult):
         self.posy = self.posy + self.speed * yMult
 
         if self.posy < 0:
             self.posy = 0
 
-        if self.posy + self.height = window_height:
-            self.posy = window_height - self.height
+        if self.posy + self.height >= self.window_height:
+            self.posy = self.window_height - self.height
 
         self.paddleRect = (self.posx, self.posy, self.width, self.height) 
 
 
-    def displayScore(self, score, x, y, color):
-        score = pg.font.Font('freesansbold.ttf', 20).render(str(score), True, (255, 255, 255))
-        scoreRect = score.get_rect()
-        scoreRect.center = (x, y)
+    def displayScore(self, score, x, y, font, color):
+        score = font.render(str(score), True, color)
+        scoreRect = score.get_rect(center=(x, y))
+
+        self.window.blit(score, scoreRect)
 
 
-    def getRect():
+    def getRect(self):
         return self.paddleRect
 
 
 
 
 class Ball:
-    def __init__(self, window, psx, posy, radius, speed, color):
+    def __init__(self, window, window_width, window_height, posx, posy, radius, base_speed, color, xMult, yMult):
         self.window = window
+        self.window_width = window_width
+        self.window_height = window_height
         self.posx = posx
         self.posy = posy
-        self.width = width
-        self.height = height
-        self.speed = speed
+        self.radius = radius
+        self.base_speed = base_speed
+        self.speed = 5 + speed_conv(base_speed)
         self.color = color
-        self.xMul = 0
-        self.yMul = 0
-        self.ball = pg.draw.circle(window, self.color, (self.posx, self.posy), self.radius)
-        self.justonce = 1
+        self.xMult = xMult
+        self.yMult = yMult
+        self.ball = pg.draw.circle(self.window, self.color, (self.posx, self.posy), self.radius)
+        self.justonce = True
+
 
     def display(self):
-        self.ball = pg.draw.circle(window, self.color, (self.posx, self.posy), self.radius)
+        self.ball = pg.draw.circle(self.window, self.color, (self.posx, self.posy), self.radius)
 
-    def update(self, window_width, window_height)
+
+    def update(self):
         
+        if self.posy - self.radius <= 0 or self.posy + self.radius >= self.window_height:
+            self.yMult *= -1
+
+        self.posx += self.speed * self.xMult
+        self.posy += self.speed * self.yMult
+        
+        #print(self.justonce, self.posx, self.window_width)
+
+        if (self.posx - self.radius) <= 0 and self.justonce:
+            self.justonce = False
+            return -1
+        elif (self.posx + self.radius) >= self.window_width and self.justonce:
+            self.justonce = False
+            return 1
+        else:
+            return 0
+
+
+    def reset(self, speed):
+        self.posx = self.window_width // 2
+        self.posy = self.window_height // 2
+
+        self.xMult *= -1
+        self.base_speed = speed
+        self.speed = 5 + speed_conv(self.base_speed)
+        self.justonce = True
+
+
+    def hit(self):
+        self.xMult *= -1
+        print(f"OLD {self.base_speed} || {self.speed}")
+        self.base_speed += 1
+        self.speed = 5 + speed_conv(self.base_speed)
+        print(f"NEW {self.base_speed} || {self.speed}")
+
+    def getRect(self):
+        return self.ball
+
+
+    def getDir(self):
+        return self.xMult
